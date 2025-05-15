af88463f7dad2ded23e814325280baf97fd0a866
Luisgugu <luis.gustavo.silva17@escola.pr.gov.br>
2025-05-15 12:13:05 -0300

    15/05 - patch 1

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,21 @@ criaCartao(
     'O que é estudo Passivo?',
     'Consiste em absorver o conteúdo, mesmo sem a nescessidade da participação direta dele.'
 )
+
+criaCartao(
+    'História',
+    'O que a História estuda?',
+    'As ações do homem ao longo do tempo'
+)
+
+criaCartao(
+    'Iluminismo',
+    'O que é o liberalismo para John Locke?',
+    'Locke argumentou que cada homem tem um direito natural à vida, à liberdade e à propriedade, complementando que os governos não devem violar esses direitos com base no contrato social.'
+)
+
+criaCartao(
+    'História',
+    'Quais eram as "rotinas tradicionais" das crianças antes do século XIX? E quem questionou isso?',
+    'A criança era um ser substituivel, logo, existia pouco afeto com a mesma. E também, ela tornava-se produtiva economicamente para a familia, logo aos 7 anos de idade. O pensador responsável por questionar isso foi Rousseau.'
+)
